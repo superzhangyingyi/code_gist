6a6426dd31f828c1c032059b479f2665d168284d
“superzhangyingyi” <zhangyingyi@scutech.com>
2023-10-30 10:14:03 +0800

    main.js转发至企业微信功能已测试成功

--- webhook_receive/main.js
+++ webhook_receive/main.js
@@ -13,15 +13,15 @@ app.post('/webhook', function(req, res){
     let headmsg = '';
     res.header('Access-Control-Allow-Origin', '*');
     res.send('accept post');
-    msg = req.body;
     headmsg = req.headers
+    msg = req.body;
+    let objBody = JSON.parse(unescape(msg.replace(/\\u/g, "%u")))
     console.log('---------------------HEAD-----------------------------');
     console.log(headmsg);
     console.log('---------------------BODY-----------------------------');
     // 解码中文
-    console.log(unescape(msg.replace(/\\u/g, "%u")));
-    // console.log(msg);
-    // WeCom_transfrom(headmsg, msg)
+    console.log(objBody);
+    WeCom_transfrom(headmsg, objBody)
 })
 
 //将迪备的请求格式转换成 企业微信-机器人推送消息的规定格式 (仅仅将body转化成字符串)
@@ -29,7 +29,7 @@ function WeCom_transfrom(head_json, body_json) {
     let newObj = {"headers":head_json, "body":body_json}
     let tpStr = JSON.stringify(newObj)
     console.log(tpStr);
-    // let sendUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2642c203-abe4-4ab4-a48a-7029390803d7'
+    let sendUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ae2acf74-3a20-449c-9ec8-6cc79aeac712'
     let json_data = {
       'msgtype': 'text',
       'text': {
